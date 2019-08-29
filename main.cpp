2beb1609cc4d42cfc9926adc4dfb76cd39fb529c
Tanishq Gupta <tanishqg2406@gmail.com>
2019-08-29 23:34:41 +0530

    Optimized and finalized the code

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ ll cc;                  // cc = conversion cost
 map<char, int> vocab;   // mapping of vocabulary to {0, 1, 2 ... |V|-1} and dash to |V|
 vector<int> act_len;    // original length of strings
 ll seed = time(0);
+int cnt_tabu = 0;
 
 inline void shuffle_vector(vector<int> &vec)
 {
@@ -26,17 +27,6 @@ public:
     int n; ll cost;
     vector<string> seq;
 
-    // returns true if dash is present in all strings at position pos
-    /*bool isDashAtPosition(int pos)
-    {
-        assert(pos < n);
-        for(int i=0;i<k;i++)
-            if(seq[i][pos] != dash)
-                return false;
-        return true;
-    }*/
-
-    // matching cost of jth character of idth string with other strings
     ll getCost(int id, int j)
     {
         assert(id < k && j < n);
@@ -55,33 +45,28 @@ public:
         cost /= 2;
         for(int i=0;i<k;i++)
             cost += (cc * (n - act_len[i]));
-        /*for(int j=0;j<n;j++)
-            if(isDashAtPosition(j))
-                cost -= (cc * k);*/
     }
 
-    // computes new cost when j1 th and j2th character of ith string are swapped
     ll computeNewCost(int i, int j1, int j2)
     {
         ll ans = cost;
-        //if(isDashAtPosition(j1)) ans += (cc * k);
-        //if(isDashAtPosition(j2)) ans += (cc * k);
         ans -= (getCost(i, j1) + getCost(i, j2));
         swap(seq[i][j1], seq[i][j2]);
-        //if(isDashAtPosition(j1)) ans -= (cc * k);
-        //if(isDashAtPosition(j2)) ans -= (cc * k);
         ans += (getCost(i, j1) + getCost(i, j2));
         swap(seq[i][j1], seq[i][j2]);
         return ans;
     }
 
-    bool moveToBestNeighbour()
+    ll getCostOfBestNeighbour(int &id, int &pos1, int &pos2)
     {
-        ll min_cost = cost, curr_cost; int id, pos1, pos2;
+        ll min_cost = LLONG_MAX, curr_cost;
         vector<int> v1(k); for(int i=0;i<k;i++) v1[i] = i;
+        vector<int> v2(n); for(int i=0;i<n;i++) v2[i] = i;
         shuffle_vector(v1);
         for(int i : v1)
-            for(int j=0;j<n;j++)
+        {
+            shuffle_vector(v2);
+            for(int j : v2)
                 if(seq[i][j] != dash)
                 {
                     int t = j-1;
@@ -107,10 +92,68 @@ public:
                         t++;
                     }
                 }
-        if(min_cost == cost) return false;
-        swap(seq[id][pos1], seq[id][pos2]);
-        cost = min_cost;
+        }
+        return min_cost;
+    }
+
+    bool isEqual(Node &other)
+    {
+        for(int i=0;i<k;i++)
+            for(int j=0;j<n;j++)
+                if(seq[i][j] != other.seq[i][j])
+                    return false;
         return true;
+    }
+
+    Node new_node(int i, int j1, int j2)
+    {
+        Node res; res.n = n; res.cost = cost;
+        res.seq = seq;
+        swap(res.seq[i][j1], res.seq[i][j2]);
+        return res;
+    }
+
+    bool isPresentInList(list<Node> &l, Node &n)
+    {
+        for(Node &tmp : l) if(n.isEqual(tmp)) return true;
+        return false;
+    }
+
+    bool moveToBestNeighbour()
+    {
+        int id, j1, j2;
+        ll min_cost = getCostOfBestNeighbour(id, j1, j2);
+        if(min_cost < cost)
+        {
+            swap(seq[id][j1], seq[id][j2]);
+            cost = min_cost;
+            return true;
+        }
+        if(min_cost > cost) return false;
+        list<Node> tabu;
+        tabu.push_back(*this);
+        Node curr_node = new_node(id, j1, j2);
+        tabu.push_back(curr_node);
+        int cnt = 0;
+        while(cnt < 100)
+        {
+            min_cost = curr_node.getCostOfBestNeighbour(id, j1, j2);
+            if(min_cost < cost)
+            {
+                cnt_tabu++;
+                swap(curr_node.seq[id][j1], curr_node.seq[id][j2]);
+                seq = curr_node.seq;
+                cost = min_cost;
+                return true;
+            }
+            if(min_cost > cost) return false;
+            curr_node = curr_node.new_node(id, j1, j2);
+            if(isPresentInList(tabu, curr_node)) return false;
+            tabu.push_back(curr_node);
+            if(tabu.size() > 50) tabu.pop_front();
+            cnt++;
+        }
+        return false;
     }
 
     bool moveToFirstBestNeighbour()
@@ -156,14 +199,7 @@ public:
 
     void print()
     {
-        //vector<int> v;
-        //for(int i=0;i<n;i++) if(!isDashAtPosition(i)) v.pb(i);
-        for(int i=0;i<k;i++)
-        {
-            //for(int j : v) cout<<seq[i][j];
-            cout<<seq[i]<<"\n";
-            //cout<<"\n";
-        }
+        for(int i=0;i<k;i++) cout<<seq[i]<<"\n";
     }
 };
 
@@ -195,7 +231,9 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    double max_time; cin>>max_time; max_time *= 60; max_time *= 0.98;
+    double max_time; cin>>max_time; max_time *= 60;
+    if(max_time > 20) max_time -= 5;
+    else max_time *= 0.95;
     cin>>v; char c; cin>>c; int N = 0;
     for(int i=0;i<v-1;i++)
     {
@@ -218,6 +256,24 @@ int main()
     ll cnt = 0;
     Node best_node = random_restart(input_str, N);
     vector<pair<ll, int>> cost_len(N+1);
+    if(N > 500)
+    {
+        int i = N+1;
+        while(((double)clock()/CLOCKS_PER_SEC) < max_time)
+        {
+            Node node = random_restart(input_str, i++);
+            while(((double)clock()/CLOCKS_PER_SEC) < max_time && node.moveToFirstBestNeighbour()) cnt++;
+            if(node.cost < best_node.cost) best_node = node;
+        }
+        best_node.print();
+    	#ifdef PROD
+        cout<<best_node.cost<<endl;
+        cout<<cnt<<endl;
+        cout<<((double)clock()/CLOCKS_PER_SEC)<<endl;
+        cout<<cnt_tabu<<"\n";
+    	#endif
+        return 0;
+    }
     for(int i=N;i<=2*N;i++)
     {
         cost_len[i-N] = {(ll)0, i};
@@ -228,6 +284,7 @@ int main()
             if(node.cost < best_node.cost) best_node = node;
             cost_len[i-N].X += node.cost;
         }
+        if(((double)clock()/CLOCKS_PER_SEC) >= max_time) break;
     }
     sort(cost_len.begin(), cost_len.end());
     vector<int> best_lens;
@@ -237,13 +294,16 @@ int main()
         for(int i : best_lens)
         {
             Node node = random_restart(input_str, i);
-            while(((double)clock()/CLOCKS_PER_SEC) < max_time && node.moveToFirstBestNeighbour()) cnt++;
+            while(((double)clock()/CLOCKS_PER_SEC) < max_time && node.moveToBestNeighbour()) cnt++;
             if(node.cost < best_node.cost) best_node = node;
         }
     }
     best_node.print();
+	#ifdef PROD
     cout<<best_node.cost<<endl;
     cout<<cnt<<endl;
     cout<<((double)clock()/CLOCKS_PER_SEC)<<endl;
+    cout<<cnt_tabu<<"\n";
+	#endif
     return 0;
 }
